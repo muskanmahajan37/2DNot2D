d35cbd0c884f8ff236a170a5f5e2ba10247d33c1
wpardliew <liew.w@husky.neu.edu>
2017-10-01 05:39:47 +0700

    level action

--- model/Level.java
+++ model/Level.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Level {
+
     public List<Wall> walls = new ArrayList<>();
 
+    public String name;
     public double playerstartx;
     public double playerstarty;
     public double playerstarttheta;
@@ -14,6 +16,20 @@ public class Level {
     public double exitX;
     public double exitY;
     public double exitRadius;
+
+
+    public Level (List<Wall> walls, double playerstartX, double playerstarty, double playerstarttheta, double exitX,
+                  double exitY, double exitRadius, String name) {
+        this.walls = walls;
+        this.playerstartx = playerstartX;
+        this.playerstarty = playerstarty;
+        this.playerstarttheta = playerstarttheta;
+        this.exitX = exitX;
+        this.exitY = exitY;
+        this.exitRadius = exitRadius;
+        this.name = name;
+    }
+
 
     public Level scale(double scale) {
         for (Wall w: walls) {
@@ -28,4 +44,6 @@ public class Level {
         exitRadius = exitRadius * scale;
         return this;
     }
+
+
 }
