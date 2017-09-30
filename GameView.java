63cfcfccb427c019d2c624691a2e240b63c8a312
Karl <KarlStephanR@gmail.com>
2017-09-29 22:36:34 -0400

    Drawing to canvas works

--- GameView.java
+++ GameView.java
@@ -3,12 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import model.Equation;
-import model.Player;
-import model.ViewLine;
-import model.Wall;
 
 public class GameView extends Application {
     public static void main(String[] args) {
@@ -17,39 +12,15 @@ public class GameView extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("2DNot2D");
 
-        Canvas canvas = new Canvas();
+        Canvas canvas = new Canvas(600, 200);
 
         GraphicsContext g = canvas.getGraphicsContext2D();
-
-
-        Player player = new Player(2, 4, 0);
-        Wall wall = new Wall(10, -500, Math.PI / 2, 1000);
-
-        int angle = -10;
-        int deltaAngle = 1;
-        int iterations = 20;
-
-        int sectionWidth = 600 / iterations;
-
-        g.setFill(Color.IVORY);
-        g.fillRect(0, 0, 10, 40);
-        for (int drawPixel = 0; drawPixel < iterations; drawPixel++) {
-            angle += deltaAngle;
-
-            ViewLine v = player.viewLine(angle);
-            Equation e = new Equation(v, wall);
-
-            g.setFill(wall.getColorAtDist(e.distanceAlongWall()));
-            g.fillRect(drawPixel * sectionWidth, 90, sectionWidth, 20 );
-        }
-
+        g.fillRect(10, 10, 10, 40);
 
         Pane root = new Pane(canvas);
-        primaryStage.setScene(new Scene(root, 600, 200));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-
     }
 }
--- TestCramer.java
+++ TestCramer.java
@@ -8,7 +8,7 @@ public class TestCramer {
         Player player = new Player(0, 0, 0);
         Wall wall = new Wall(10, -5, Math.PI / 2, 1000);
 
-        Equation e = new Equation(player.viewLine(), wall);
+        Equation e = new Equation(player.viewLine(0), wall);
 
         System.out.println(e.distanceAlongViewLine());
         System.out.println(e.distanceAlongWall());
